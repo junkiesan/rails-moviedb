52cc1043ee1bc825f48fc3254812ae0e35ca79db
Julien Bouland <bouland.julien@gmail.com>
2021-03-28 19:11:16 +0200

    pg_search: initialized in movie and tv show models

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,4 @@
 class MoviesController < ApplicationController
-  include PgSearch::Model
-  pg_search_scope :search_by_title_and_synopsis,
-    against: [ :title, :synopsis ],
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    }
-
   def index
     @movies = Movie.all
     if params[:query].present?
